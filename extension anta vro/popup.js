af228af69eaacc144c193a3519491da82c04ad44
PreranaYekkele <115355885+PreranaYekkele@users.noreply.github.com>
2024-01-16 13:35:00 +0530

    Add files via upload

---
+++ extension anta vro/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function detectDarkPatterns() {
+      alert('Dark patterns bust hogaye chalo bye');
+    }
+    document.getElementById('detectButton').addEventListener('click', detectDarkPatterns);
+  });
+  
