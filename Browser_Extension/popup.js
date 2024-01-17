3bdadb7fc436732950ff41decd60faba5310cbce
PreranaYekkele <115355885+PreranaYekkele@users.noreply.github.com>
2024-01-17 18:25:26 +0530

    Add files via upload

---
+++ Browser_Extension/popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function detectDarkPatterns() {
+      alert('Flagged Detected Patterns');
+    }
+    document.getElementById('detectButton').addEventListener('click', detectDarkPatterns);
+  });
+  
+
+ 
