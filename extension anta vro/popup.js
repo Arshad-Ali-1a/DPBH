8690879a62cf9d2bbb4a6134b94158b203d2aff4
PreranaYekkele <115355885+PreranaYekkele@users.noreply.github.com>
2024-01-24 07:44:03 +0530

    Delete extension anta vro directory

--- extension anta vro/popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    function detectDarkPatterns() {
-      alert('Dark patterns bust hogaye chalo bye');
-    }
-    document.getElementById('detectButton').addEventListener('click', detectDarkPatterns);
-  });
-  
