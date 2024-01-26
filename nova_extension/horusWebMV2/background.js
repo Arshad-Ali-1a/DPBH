c01eaef2e8216f5138acdd75f83184fa6000f6a6
srujan landeri <srujanlanderi@gmail.com>
2024-01-26 12:20:01 +0530

    update scrapper

--- nova_extension/horusWebMV2/background.js
+++
@@ -1,8 +0,0 @@
-chrome.commands.onCommand.addListener(function(command) {
-  if (command === "getDOM") {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: "getDOM" });
-          console.log("Message sent");
-      });
-  }
-});
